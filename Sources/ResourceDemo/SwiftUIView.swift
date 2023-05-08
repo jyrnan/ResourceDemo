c018539972319ef7c8b62bb2d466849eaa151687
jyrnan <yoeking@gmail.com>
2023-05-08 16:13:25 +0800

    add color

--- Sources/ResourceDemo/SwiftUIView.swift
+++ Sources/ResourceDemo/SwiftUIView.swift
@@ -9,9 +9,18 @@ import SwiftUI
 
 public struct SwiftUIView: View {
   public var body: some View {
-    Image("隐私协议_勾选提示_bg", bundle: Bundle.module)
     
-    }
+    Image("隐私协议_勾选提示_bg", bundle: Bundle.module)
+      .resizable()
+      .aspectRatio(contentMode: .fit)
+      .overlay {
+        Text("Hello world")
+          .font(.title)
+          .bold()
+          .foregroundColor(.init("TextColor1", bundle: Bundle.module))
+      }
+  }
+  
   
   public init() {
   }
