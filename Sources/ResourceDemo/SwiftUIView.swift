357a61e52711debbb20609562b98c7524baf2f36
jyrnan <yoeking@gmail.com>
2023-05-08 15:38:03 +0800

    add public to init

--- Sources/ResourceDemo/SwiftUIView.swift
+++ Sources/ResourceDemo/SwiftUIView.swift
@@ -11,6 +11,9 @@ public struct SwiftUIView: View {
   public var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
+  
+  public init() {
+  }
 }
 
 struct SwiftUIView_Previews: PreviewProvider {
